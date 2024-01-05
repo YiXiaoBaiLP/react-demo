df0907f6bbef1200cf9366838761c908c656af0f
YiXiaoBai <yixiaobai.lp@qq.com>
2024-01-05 12:12:18 +0800

    将 项目进行备份

---
+++ 备份/01.React 项目说明/src/App.js
@@ -0,0 +1,13 @@
+/**
+ * 项目根组件
+ * @returns 
+ */
+function App() {
+  return (
+    <div className="App">
+      this is App
+    </div>
+  );
+}
+
+export default App;
---
+++ 备份/01.React 项目说明/src/index.js
@@ -0,0 +1,19 @@
+/**
+ * 项目的入口，从这里开始运行
+ * 渲染流程
+ * App ->index.js -> public/index.html(root)页面节点上
+ */
+// React必要的两个核心包
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// 导入 项目的根组件
+import App from './App';
+
+// 把 App 根组件渲染到id 为 root 的dom节点上
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
