ab85a89cabb738a91ad3be0de1ff316d585386a1
YiXiaoBai <yixiaobai.lp@qq.com>
2024-01-07 15:37:21 +0800

    使用指定一 css 样式

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import "./index.css";
+
+// 定义一个样式对象
+const style = {
+  color: "red",
+  fontSize: "100px",
+};
+
 /**
+ * 使用自定义的样式文件
  * 项目根组件
  * @returns
  */
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* 不推荐此方式 */}
+      {/* <span style={{ color: "blue", fontSize: "100px" }}> */}
+      <span style={style}>这是一个自定义的组件样式</span>
+      <br />
+      {/* 使用 导入第三方文件的方式 */}
+      {/* 需要在className中使用所定义的样式名称 */}
+      <span className="foo">使用第三方文件的方式</span>
+    </div>
+  );
 }
 
 export default App;
