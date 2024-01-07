db3b53b297ab50a270c2af3fb575ae462fb52126
YiXiaoBai <yixiaobai.lp@qq.com>
2024-01-07 17:17:02 +0800

    使用 useRef 来获取原生 DOM

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import "./index.css";
 
 // 定义一个样式对象
@@ -12,6 +13,15 @@ const style = {
  * @returns
  */
 function App() {
+  // 1 使用useRef 生成ref对象，绑定到dom标签身上
+  // 2 dom可用时，ref。current获取dom
+  // 渲染完毕之后dom生成之后才可用
+  const inputRef = useRef(null); // 初始化时默认传入 null
+  const showDom = () => {
+    console.dir(inputRef.current);
+    // 获取到文本框中输入的值
+    console.log(inputRef.current.value);
+  };
   return (
     <div className="App">
       {/* 不推荐此方式 */}
@@ -21,6 +31,9 @@ function App() {
       {/* 使用 导入第三方文件的方式 */}
       {/* 需要在className中使用所定义的样式名称 */}
       <span className="foo">使用第三方文件的方式</span>
+      {/* 将 ref 对象绑定到dom标签上 */}
+      <input type="text" ref={inputRef}></input>
+      <button onClick={showDom}>获取Dom元素</button>
     </div>
   );
 }
