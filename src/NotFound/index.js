506986430bcb738f9834ec18247f634db5462a39
YiXiaoBai <yixiaobai.lp@qq.com>
2024-02-21 23:07:49 +0800

    router 中配置 404页面

---
+++ src/NotFound/index.js
@@ -0,0 +1,4 @@
+const NotFound = () => {
+  return <div>你所访问的页面已经找不到了</div>;
+};
+export default NotFound;
---
+++ 备份/23-2. Router 中设置404页面/src/page/Layout/index.js
@@ -0,0 +1,16 @@
+import { Outlet, Link } from "react-router-dom";
+const Layout = () => {
+  return (
+    <div>
+      我是布局页面
+      <br />
+      <Link to="/board">打开看板页面</Link>
+      <br />
+      <Link to="/">打开关于页面</Link>
+      {/* 这里是二级标签页面显示的位置 */}
+      <Outlet />
+    </div>
+  );
+};
+
+export default Layout;
